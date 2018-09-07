e586aa282b62fd42d36f93b1f553e759c9587ace
Jay Sharma <jay.sharma@samsung.com>
2018-09-07 17:27:38 +0530

    Implemented unit test cases for secured mode

--- samples/ezmqx-topicdiscovery/src/main/java/org/ezmqx/topicdiscovery/TopicDiscovery.java
+++ samples/ezmqx-topicdiscovery/src/main/java/org/ezmqx/topicdiscovery/TopicDiscovery.java
@@ -18,101 +18,104 @@
 package org.ezmqx.topicdiscovery;
 
 import java.util.List;
-
 import org.protocol.ezmqx.EZMQXConfig;
 import org.protocol.ezmqx.EZMQXException;
 import org.protocol.ezmqx.EZMQXTopic;
 import org.protocol.ezmqx.EZMQXTopicDiscovery;
 
 public class TopicDiscovery {
-	public static final String TNS_CONFIG_FILE_PATH = "tnsConf.json";
+  public static final String TNS_CONFIG_FILE_PATH = "tnsConf.json";
 
-	private static void printError() {
-		System.out.println("\nRe-run the application as shown in below example: ");
-		System.out.println("\n  (1) For running in standalone mode: ");
-		System.out.println("      $ java -jar ezmqx-topicdiscovery-sample.jar -t /a/b/c -tns 192.168.8.6");
-		System.out.println("\n  (2) For running in docker mode: ");
-		System.out.println("      $ java -jar ezmqx-topicdiscovery-sample.jar -t /a/b/c");
-		System.exit(-1);
-	}
+  private static void printError() {
+    System.out.println("\nRe-run the application as shown in below example: ");
+    System.out.println("\n  (1) For running in standalone mode: ");
+    System.out
+        .println("      $ java -jar ezmqx-topicdiscovery-sample.jar -t /a/b/c -tns 192.168.8.6");
+    System.out.println("\n  (2) For running in docker mode: ");
+    System.out.println("      $ java -jar ezmqx-topicdiscovery-sample.jar -t /a/b/c");
+    System.out
+        .println("\nNote: docker mode will work only when sample is running in docker container");
+    System.exit(-1);
+  }
 
-	public static void printTopicList(List<EZMQXTopic> topicList) {
-		if (0 == topicList.size()) {
-			System.out.println("Topic list is empty.....");
-			return;
-		}
-		for (EZMQXTopic ezmqXTopic : topicList) {
-			System.out.println("=================================================");
-			System.out.println("Topic: " + ezmqXTopic.getName());
-			System.out.println("Endpoint: " + ezmqXTopic.getEndPoint().toString());
-			System.out.println("Data Model: " + ezmqXTopic.getDatamodel());
-			System.out.println("=================================================");
-		}
-	}
+  public static void printTopicList(List<EZMQXTopic> topicList) {
+    if (0 == topicList.size()) {
+      System.out.println("Topic list is empty.....");
+      return;
+    }
+    for (EZMQXTopic ezmqXTopic : topicList) {
+      System.out.println("=================================================");
+      System.out.println("Topic: " + ezmqXTopic.getName());
+      System.out.println("Endpoint: " + ezmqXTopic.getEndPoint().toString());
+      System.out.println("Data Model: " + ezmqXTopic.getDatamodel());
+      System.out.println("Is secured: " + ezmqXTopic.isSecured());
+      System.out.println("=================================================");
+    }
+  }
 
-	public static void main(String[] args) {
-		if (args.length != 2 && args.length != 4) {
-			printError();
-		}
-		int n = 0;
-		String topic = null;
-		String tnsAddress = null;
-		boolean isStandAlone = false;
-		while (n < args.length) {
-			if (args[n].equalsIgnoreCase("-t")) {
-				topic = args[n + 1];
-				System.out.println("Topic is : " + topic);
-				n = n + 2;
-			} else if (args[n].equalsIgnoreCase("-tns")) {
-				tnsAddress = args[n + 1];
-				System.out.println("TNS address is : " + tnsAddress);
-				n = n + 2;
-				isStandAlone = true;
-			} else {
-				printError();
-			}
-		}
+  public static void main(String[] args) {
+    if (args.length != 2 && args.length != 4) {
+      printError();
+    }
+    int n = 0;
+    String topic = null;
+    String tnsAddress = null;
+    boolean isStandAlone = false;
+    while (n < args.length) {
+      if (args[n].equalsIgnoreCase("-t")) {
+        topic = args[n + 1];
+        System.out.println("Topic is : " + topic);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-tns")) {
+        tnsAddress = args[n + 1];
+        System.out.println("TNS address is : " + tnsAddress);
+        n = n + 2;
+        isStandAlone = true;
+      } else {
+        printError();
+      }
+    }
 
-		EZMQXConfig config = null;
-		try {
-			config = EZMQXConfig.getInstance();
-			if (isStandAlone) {
-				config.startStandAloneMode("", true, tnsAddress);
-			} else {
-				config.startDockerMode(TNS_CONFIG_FILE_PATH);
-			}
-			System.out.println("Config done");
-		} catch (EZMQXException e) {
-			System.out.println("Config exception Errorcode: " + e.getCode() + "  Message: " + e.getMsg());
-			return;
-		}
+    EZMQXConfig config = null;
+    try {
+      config = EZMQXConfig.getInstance();
+      if (isStandAlone) {
+        config.startStandAloneMode("localhost", true, "http://" + tnsAddress + ":80/tns-server");
+      } else {
+        config.startDockerMode(TNS_CONFIG_FILE_PATH);
+      }
+      System.out.println("Config done");
+    } catch (EZMQXException e) {
+      System.out.println("Config exception Errorcode: " + e.getCode() + "  Message: " + e.getMsg());
+      return;
+    }
 
-		List<EZMQXTopic> topicList;
-		try {
-			EZMQXTopicDiscovery topicDiscovery = new EZMQXTopicDiscovery();
-			topicList = topicDiscovery.hierarchicalQuery(topic);
-		} catch (EZMQXException e) {
-			System.out.println("Caught exception: " + e.getMsg());
-			return;
-		}
-		printTopicList(topicList);
+    List<EZMQXTopic> topicList;
+    try {
+      EZMQXTopicDiscovery topicDiscovery = new EZMQXTopicDiscovery();
+      topicList = topicDiscovery.hierarchicalQuery(topic);
+    } catch (EZMQXException e) {
+      System.out.println("Caught exception: " + e.getMsg());
+      return;
+    }
+    printTopicList(topicList);
 
-		// In docker mode, wait for 5 minutes before exit [For docker mode].
-		if (!isStandAlone) {
-			try {
-				System.out
-						.println("Waiting for 5 minutes before program exit for docker mode... [press ctrl+c to exit]");
-				Thread.sleep(300000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+    // In docker mode, wait for 5 minutes before exit [For docker mode].
+    if (!isStandAlone) {
+      try {
+        System.out.println(
+            "Waiting for 5 minutes before program exit for docker mode... [press ctrl+c to exit]");
+        Thread.sleep(300000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
 
-		try {
-			config.reset();
-			System.out.println("Reset Config done!!");
-		} catch (EZMQXException e) {
-			e.printStackTrace();
-		}
-	}
+    try {
+      config.reset();
+      System.out.println("Reset Config done!!");
+    } catch (EZMQXException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- samples/ezmqx-xmlsubscriber/src/main/java/org/ezmqx/subscriber/XmlSubscriber.java
+++ samples/ezmqx-xmlsubscriber/src/main/java/org/ezmqx/subscriber/XmlSubscriber.java
@@ -21,139 +21,212 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
+import org.protocol.ezmqx.EZMQXAmlSubscriber;
 import org.protocol.ezmqx.EZMQXConfig;
 import org.protocol.ezmqx.EZMQXEndPoint;
 import org.protocol.ezmqx.EZMQXErrorCode;
 import org.protocol.ezmqx.EZMQXException;
 import org.protocol.ezmqx.EZMQXTopic;
+import org.protocol.ezmqx.EZMQXTopicDiscovery;
 import org.protocol.ezmqx.EZMQXXmlSubscriber;
 import org.protocol.ezmqx.EZMQXXmlSubscriber.EZMQXXmlSubCallback;
 
 public class XmlSubscriber {
-	public static EZMQXConfig mConfig;
-	public static EZMQXXmlSubscriber mSubscriber;
-	public static Lock terminateLock = new ReentrantLock();
-	public static java.util.concurrent.locks.Condition condVar = terminateLock.newCondition();
-	public static final String AML_FILE_PATH = "sample_data_model.aml";
-	public static final String TNS_CONFIG_FILE_PATH = "tnsConf.json";
-	public static final String LOCAL_HOST = "localhost";
+  public static EZMQXConfig mConfig;
+  public static EZMQXXmlSubscriber mSubscriber;
+  public static Lock terminateLock = new ReentrantLock();
+  public static java.util.concurrent.locks.Condition condVar = terminateLock.newCondition();
+  public static final String AML_FILE_PATH = "sample_data_model.aml";
+  public static final String TNS_CONFIG_FILE_PATH = "tnsConf.json";
+  public static final String LOCAL_HOST = "localhost";
+  private static String mServerPublicKey = "tXJx&1^QE2g7WCXbF.$$TVP.wCtxwNhR8?iLi&S<";
+  private static String mClientPublicKey = "-QW?Ved(f:<::3d5tJ$[4Er&]6#9yr=vha/caBc(";
+  private static String mClientSecretKey = "ZB1@RS6Kv^zucova$kH(!o>tZCQ.<!Q)6-0aWFmW";
 
-	public static EZMQXXmlSubCallback mCallback = new EZMQXXmlSubCallback() {
-		public void onMessage(String topic, String data) {
-			System.out.println("[APP Callback] Topic: " + topic + "  data: " + data);
-		}
+  public static EZMQXXmlSubCallback mCallback = new EZMQXXmlSubCallback() {
+    public void onMessage(String topic, String data) {
+      System.out.println("[APP Callback] Topic: " + topic + "  data: " + data);
+    }
 
-		public void onError(String topic, EZMQXErrorCode errorCode) {
-			System.out.println("[APP Callback] Topic: " + topic + "  Errorcode: " + errorCode);
-		}
-	};
+    public void onError(String topic, EZMQXErrorCode errorCode) {
+      System.out.println("[APP Callback] Topic: " + topic + "  Errorcode: " + errorCode);
+    }
+  };
 
-	private static void printError() {
-		System.out.println("\nRe-run the application as shown in below example: ");
-		System.out.println("\n  (1) For running in standalone mode: ");
-		System.out.println("      $ java -jar ezmqx-xmlsubscriber-sample.jar -ip 192.168.1.1 -port 5562 -t /topic");
-		System.out.println("\n  (2) For running in docker mode: ");
-		System.out.println("      $ java -jar ezmqx-xmlsubscriber-sample.jar -t /topic -h true");
-		System.out.println("\n Note: -h stands for hierarchical search for topic from TNS server");
-		System.exit(-1);
-	}
+  private static String getTNSAddres(String address) {
+    return "http://" + address + ":80/tns-server";
+  }
 
-	public static void main(String[] args) {
-		if (args.length != 4 && args.length != 6) {
-			printError();
-		}
+  private static void printError() {
+    System.out.println("\nRe-run the application as shown in below example: ");
+    System.out.println("\n  (1) For running in standalone mode: ");
+    System.out.println(
+        "      $ java -jar ezmqx-xmlsubscriber-sample.jar -ip 192.168.1.1 -port 5562 -t /topic");
+    System.out.println("\n  (2) For running in standalone mode [Secured]: ");
+    System.out.println(
+        "      $ java -jar ezmqx-xmlsubscriber-sample.jar -ip 192.168.1.1 -port 5562 -t /topic -secured 1");
+    System.out.println("\n  (3) For running in standalone mode: [With TNS] ");
+    System.out
+        .println("      $ java -jar ezmqx-xmlsubscriber-sample.jar -t /topic -tns 192.168.10.1");
+    System.out.println("\n  (4) For running in standalone mode [With TNS + Secured]: ");
+    System.out.println(
+        "      $ java -jar ezmqx-xmlsubscriber-sample.jar -t /topic -tns 192.168.10.1 -secured 1 ");
+    System.out.println("\n  (5) For running in docker mode: ");
+    System.out.println("      $ java -jar ezmqx-xmlsubscriber-sample.jar -t /topic -h true");
+    System.out.println("\n  (6) For running in docker mode [Secured]: ");
+    System.out
+        .println("      $ java -jar ezmqx-xmlsubscriber-sample.jar -t /topic -h true -secured 1");
+    System.out.println("\nNote:");
+    System.out.println("  (1) -h stands for hierarchical search for topic from TNS server");
+    System.out
+        .println("  (2) docker mode will work only when sample is running in docker container");
 
-		int n = 0;
-		String topic = null;
-		String ip = null;
-		String hierarchical = null;
-		int port = 0;
-		boolean isStandAlone = false;
-		while (n < args.length) {
-			if (args[n].equalsIgnoreCase("-ip")) {
-				ip = args[n + 1];
-				System.out.println("Given Ip: " + ip);
-				n = n + 2;
-				isStandAlone = true;
-			} else if (args[n].equalsIgnoreCase("-port")) {
-				port = Integer.parseInt(args[n + 1]);
-				System.out.println("Given Port: " + port);
-				n = n + 2;
-			} else if (args[n].equalsIgnoreCase("-t")) {
-				topic = args[n + 1];
-				System.out.println("Topic is : " + topic);
-				n = n + 2;
-			} else if (args[n].equalsIgnoreCase("-h")) {
-				hierarchical = args[n + 1];
-				System.out.println("Is hierarchical : " + hierarchical);
-				n = n + 2;
-			} else {
-				printError();
-			}
-		}
+    System.exit(-1);
+  }
 
-		// handle command line ctrl+c signal, terminate subscriber
-		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-			public void run() {
-				try {
-					if (null != mSubscriber) {
-						mSubscriber.terminate();
-						System.out.println("Terminated subscriber!!");
-					}
-					if (null != mConfig) {
-						mConfig.reset();
-						System.out.println("Reset Config done!!");
-					}
-					terminateLock.lock();
-					condVar.signalAll();
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					terminateLock.unlock();
-				}
-			}
-		}));
+  public static void main(String[] args) {
+    if (args.length != 4 && args.length != 6 && args.length != 8) {
+      printError();
+    }
 
-		try {
-			mConfig = EZMQXConfig.getInstance();
-			if (isStandAlone) {
-				mConfig.startStandAloneMode(LOCAL_HOST, false, "");
-			} else {
-				mConfig.startDockerMode(TNS_CONFIG_FILE_PATH);
-			}
-			System.out.println("Config done");
+    int n = 0;
+    String topic = null;
+    String ip = null;
+    String hierarchical = null;
+    String tnsAddr = null;
+    int port = 0;
+    int isSecured = 0;
+    boolean isStandAlone = false;
+    while (n < args.length) {
+      if (args[n].equalsIgnoreCase("-ip")) {
+        ip = args[n + 1];
+        System.out.println("Given Ip: " + ip);
+        n = n + 2;
+        isStandAlone = true;
+      } else if (args[n].equalsIgnoreCase("-port")) {
+        port = Integer.parseInt(args[n + 1]);
+        System.out.println("Given Port: " + port);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-t")) {
+        topic = args[n + 1];
+        System.out.println("Topic is : " + topic);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-h")) {
+        hierarchical = args[n + 1];
+        System.out.println("Is hierarchical : " + hierarchical);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-secured")) {
+        isSecured = Integer.parseInt(args[n + 1]);
+        System.out.println("Is Secured: " + isSecured);
+        n = n + 2;
+      } else if (args[n].equalsIgnoreCase("-tns")) {
+        tnsAddr = args[n + 1];
+        System.out.println("TNS Address : " + tnsAddr);
+        n = n + 2;
+        isStandAlone = true;
+      } else {
+        printError();
+      }
+    }
 
-			List<String> amlFilePath = new ArrayList<String>();
-			amlFilePath.add(AML_FILE_PATH);
-			List<String> IdList = mConfig.addAmlModel(amlFilePath);
-			System.out.println("Id is: " + IdList.get(0));
+    // handle command line ctrl+c signal, terminate subscriber
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      public void run() {
+        try {
+          if (null != mSubscriber) {
+            mSubscriber.terminate();
+            System.out.println("Terminated subscriber!!");
+          }
+          if (null != mConfig) {
+            mConfig.reset();
+            System.out.println("Reset Config done!!");
+          }
+          terminateLock.lock();
+          condVar.signalAll();
+        } catch (Exception e) {
+          e.printStackTrace();
+        } finally {
+          terminateLock.unlock();
+        }
+      }
+    }));
 
-			if (isStandAlone) {
-				EZMQXEndPoint endPoint = new EZMQXEndPoint(ip, port);
-				EZMQXTopic topic1 = new EZMQXTopic(topic, IdList.get(0), endPoint);
-				mSubscriber = EZMQXXmlSubscriber.getSubscriber(topic1, mCallback);
-			} else {
-				mSubscriber = EZMQXXmlSubscriber.getSubscriber(topic, true, mCallback);
-			}
-			if (null == mSubscriber) {
-				System.out.println("Subscriber is null");
-				return;
-			}
-			System.out.println("Suscribed to publisher.. -- Waiting for Events -- [press ctrl+c to exit]");
-		} catch (EZMQXException e) {
-			System.out.println("[App] Exception occured [Errorcode]: " + e.getCode() + "  [Message]: " + e.getMsg());
-			return;
-		}
+    try {
+      mConfig = EZMQXConfig.getInstance();
+      if (isStandAlone) {
+        if (null == tnsAddr) {
+          mConfig.startStandAloneMode("", false, "");
+        } else {
+          mConfig.startStandAloneMode("", true, getTNSAddres(tnsAddr));
+        }
+      } else {
+        mConfig.startDockerMode(TNS_CONFIG_FILE_PATH);
+      }
+      System.out.println("Config done");
 
-		// Prevent main thread from exit
-		try {
-			terminateLock.lock();
-			condVar.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			terminateLock.unlock();
-		}
-	}
+      List<String> amlFilePath = new ArrayList<String>();
+      amlFilePath.add(AML_FILE_PATH);
+      List<String> IdList = mConfig.addAmlModel(amlFilePath);
+      System.out.println("Id is: " + IdList.get(0));
+
+      if (isStandAlone) {
+        if (0 == isSecured) {
+          if (null == tnsAddr) {
+            EZMQXEndPoint endPoint = new EZMQXEndPoint(ip, port);
+            EZMQXTopic topic1 = new EZMQXTopic(topic, IdList.get(0), false, endPoint);
+            mSubscriber = EZMQXXmlSubscriber.getSubscriber(topic1, mCallback);
+          } else {
+            mSubscriber = EZMQXXmlSubscriber.getSubscriber(topic, true, mCallback);
+          }
+        } else {
+          //Topic discovery
+          if (null == tnsAddr) {
+            EZMQXEndPoint endPoint = new EZMQXEndPoint(ip, port);
+            EZMQXTopic topic1 = new EZMQXTopic(topic, IdList.get(0), true, endPoint);
+            mSubscriber = EZMQXXmlSubscriber.getSecuredSubscriber(topic1, mServerPublicKey,
+                mClientPublicKey, mClientSecretKey, mCallback);
+          } else {
+            EZMQXTopicDiscovery topicDiscovery = new EZMQXTopicDiscovery();
+            EZMQXTopic tdTopic = topicDiscovery.query(topic);
+            EZMQXTopic topic1 = new EZMQXTopic(tdTopic.getName(), tdTopic.getDatamodel(),
+                tdTopic.isSecured(), tdTopic.getEndPoint());
+            mSubscriber = EZMQXXmlSubscriber.getSecuredSubscriber(topic1, mServerPublicKey,
+                mClientPublicKey, mClientSecretKey, mCallback);
+          }
+        }
+      } else {
+        if (0 == isSecured) {
+          mSubscriber = EZMQXXmlSubscriber.getSubscriber(topic, true, mCallback);
+        } else {
+          // Do topic discovery to get end point info
+          EZMQXTopicDiscovery topicDiscovery = new EZMQXTopicDiscovery();
+          EZMQXTopic tdTopic = topicDiscovery.query(topic);
+          EZMQXTopic topic1 = new EZMQXTopic(tdTopic.getName(), tdTopic.getDatamodel(),
+              tdTopic.isSecured(), tdTopic.getEndPoint());
+          mSubscriber = EZMQXXmlSubscriber.getSecuredSubscriber(topic1, mServerPublicKey,
+              mClientPublicKey, mClientSecretKey, mCallback);
+        }
+      }
+      if (null == mSubscriber) {
+        System.out.println("Subscriber is null");
+        return;
+      }
+      System.out
+          .println("Suscribed to publisher.. -- Waiting for Events -- [press ctrl+c to exit]");
+    } catch (EZMQXException e) {
+      System.out.println(
+          "[App] Exception occured [Errorcode]: " + e.getCode() + "  [Message]: " + e.getMsg());
+      return;
+    }
+
+    // Prevent main thread from exit
+    try {
+      terminateLock.lock();
+      condVar.await();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      terminateLock.unlock();
+    }
+  }
 }
